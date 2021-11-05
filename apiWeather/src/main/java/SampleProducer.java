b8bd49d288d920cbd0d285cc83ad618b6b2414de
Zayn-6point6 <zayn.rassam@6point6.co.uk>
2021-11-05 17:17:51 +0000

    SampleProducer Value Change

--- apiWeather/src/main/java/SampleProducer.java
+++ apiWeather/src/main/java/SampleProducer.java
@@ -21,7 +21,7 @@ public class SampleProducer {
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
         //Create Producer Record
-        ProducerRecord<String, String> record = new ProducerRecord<String,String>("weather_topic", "helloooo");
+        ProducerRecord<String, String> record = new ProducerRecord<String,String>("weather_topic", "17:15");
 
         //Send data - asynchronous
         producer.send(record);
